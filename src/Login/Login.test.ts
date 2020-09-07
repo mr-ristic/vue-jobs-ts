25f24063733ec9310690544276d0b361031d222e
mr-ristic <marko.risticcc@gmail.com>
2020-09-07 18:40:28 +0200

    Render Login input fields

--- src/Login/Login.test.ts
+++ src/Login/Login.test.ts
@@ -1,9 +1,22 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import Login from '.';
 
 describe('Login.vue tests', () => {
   it('should render Login component', () => {
     const wrapper = shallowMount(Login);
     expect(wrapper).toBeTruthy();
+  });
+
+  it('should render Login form inputs', () => {
+    const wrapper = mount(Login);
+
+    const emailInput = wrapper.get('#email');
+    const passwordInput = wrapper.get('#password');
+    const submit = wrapper.get('button');
+
+    expect(emailInput.attributes('name')).toEqual('email');
+    expect(passwordInput.attributes('name')).toEqual('password');
+    expect(submit.text()).toEqual('Sign in');
+    wrapper.destroy();
   });
 });
--- src/Login/Login.vue
+++ src/Login/Login.vue
@@ -1,6 +1,43 @@
 <template>
-  <div>
-    Login
+  <div class="d-flex justify-content-center">
+    <div class="mt-4 col-4">
+      <form @submit.prevent="onSubmit">
+        <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+        <div class="form-group row">
+          <label for="email" class="sr-only">Email address</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            class="form-control"
+            placeholder="Email address"
+            autocomplete="on"
+            v-model="email"
+          />
+          <small id="email-error" class="text-danger">
+            Error placeholder
+          </small>
+        </div>
+        <div class="form-group row">
+          <label for="password" class="sr-only">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            class="form-control mt-3 is-invalid"
+            placeholder="Password"
+            autocomplete="on"
+            v-model="password"
+          />
+          <small class="text-danger">
+            Error placeholder
+          </small>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
